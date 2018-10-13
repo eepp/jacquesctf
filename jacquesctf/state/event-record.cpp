83a7967bae8be6c86aaf1940d265b84fd4a287e9
Philippe Proulx <eeppeliteloop@gmail.com>
2018-10-13 11:34:18 -0400

    EventRecord::createFromPacketSequenceIterator(): simplify
    yactfr guarantees that an event record beginning's offset is the same as
    its first scope (which is the same as its first structure, which is
    aligned).
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- jacquesctf/state/event-record.cpp
+++ jacquesctf/state/event-record.cpp
@@ -41,37 +41,8 @@ EventRecord::SP EventRecord::createFromPacketSequenceIterator(yactfr::PacketSequ
         // TODO: replace with element visitor
         switch (it->kind()) {
         case yactfr::Element::Kind::EVENT_RECORD_BEGINNING:
-        {
-            /*
-             * Find the first structure to skip the padding before the
-             * event record, so as to get an expected offset.
-             */
-            auto isDone = false;
-
-            while (!isDone) {
-                switch (it->kind()) {
-                case yactfr::Element::Kind::STRUCT_BEGINNING:
-                    segment.offsetInPacketBits(it.offset() - packetOffsetInDataStreamBits);
-                    isDone = true;
-                    break;
-
-                case yactfr::Element::Kind::EVENT_RECORD_TYPE:
-                {
-                    auto& elem = static_cast<const yactfr::EventRecordTypeElement&>(*it);
-
-                    eventRecordType = &elem.eventRecordType();
-                    break;
-                }
-
-                default:
-                    break;
-                }
-
-                ++it;
-            }
-
+            segment.offsetInPacketBits(it.offset() - packetOffsetInDataStreamBits);
             break;
-        }
 
         case yactfr::Element::Kind::EVENT_RECORD_TYPE:
         {
