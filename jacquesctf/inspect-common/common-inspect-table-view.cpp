ed413a5bfcdb56312072fae905a4f07659cc564a
Philippe Proulx <eeppeliteloop@gmail.com>
2022-10-07 13:10:12 -0400

    Add `jacques::CommonInspectTableView` base class
    Any table view may now inherit `jacques::CommonInspectTableView` to
    access common packet inspection table view features such as first
    visible row management, selection, up/down and page up/down actions,
    center on row action, and the rest.
    
    `jacques::CommonInspectTableView` may also serve as the base of other
    GUI table views.
    
    `jacques::CommonInspectTableView` only requires the derived class to
    call its _updateCounts() method with the current total row count and
    maximum number of visible rows (the window of visible rows within the
    table).
    
    Initially, the total row count is zero. Most methods expect the total
    row count to be greater than zero.
    
    All the action methods return whether or not there were changes caused
    by the requested action. For example, _pageUp() returns true if the
    first visible row (_firstVisibleRow()) changed.
    
    The _sel*Row() methods accept an optional `ensureVisible` parameter
    which, if true, will make the method also update the first visible row
    if needed to make the new selected row visible.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ jacquesctf/inspect-common/common-inspect-table-view.cpp
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2022 Philippe Proulx <eepp.ca> - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium, is strictly
+ * prohibited. Proprietary and confidential.
+ */
+
+#include <cassert>
+#include <algorithm>
+
+#include "common-inspect-table-view.hpp"
+
+namespace jacques {
+
+void CommonInspectTableView::_updateCounts(const Size rowCount, const Size maxVisibleRowCount)
+{
+    if (rowCount != _theRowCount) {
+        _theFirstVisibleRow = 0;
+        _theSelRow = 0;
+    }
+
+    _theRowCount = rowCount;
+    _theMaxVisibleRowCount = maxVisibleRowCount;
+
+    if (_theRowCount > 0) {
+        _theFirstVisibleRow = std::min(_theFirstVisibleRow, this->_maxFirstVisibleRow());
+    }
+}
+
+bool CommonInspectTableView::_firstVisibleRow(const Index firstVisibleRow)
+{
+    assert(_theRowCount > 0);
+    assert(firstVisibleRow <= this->_maxFirstVisibleRow());
+
+    if (firstVisibleRow == _theFirstVisibleRow) {
+        return false;
+    }
+
+    _theFirstVisibleRow = firstVisibleRow;
+    return true;
+}
+
+CommonInspectTableView::_Change CommonInspectTableView::_selFirstRow(const bool ensureVisible)
+{
+    assert(_theRowCount > 0);
+    return this->_selRow(0, ensureVisible);
+}
+
+CommonInspectTableView::_Change CommonInspectTableView::_selLastRow(const bool ensureVisible)
+{
+    assert(_theRowCount > 0);
+    return this->_selRow(_theRowCount - 1, ensureVisible);
+}
+
+CommonInspectTableView::_Change CommonInspectTableView::_selNextRow(const Size steps,
+                                                                    const bool ensureVisible)
+{
+    return this->_selRow(steps, ensureVisible, [this] {
+        return std::min(_theSelRow + 1, _theRowCount - 1);
+    });
+}
+
+CommonInspectTableView::_Change CommonInspectTableView::_selPrevRow(const Size steps,
+                                                                    const bool ensureVisible)
+{
+    return this->_selRow(steps, ensureVisible, [this] {
+        return _theSelRow == 0 ? 0 : _theSelRow - 1;
+    });
+}
+
+CommonInspectTableView::_Change CommonInspectTableView::_selRow(const Index row,
+                                                                const bool ensureVisible)
+{
+    if (_theRowCount == 0) {
+        return _Change::NONE;
+    }
+
+    if (row == _theSelRow) {
+        return _Change::NONE;
+    }
+
+    _theSelRow = row;
+
+    if (ensureVisible && !this->_rowIsVisible(row)) {
+        if (row < _theFirstVisibleRow) {
+            _theFirstVisibleRow = row;
+        } else {
+            _theFirstVisibleRow = row - _theMaxVisibleRowCount + 1;
+        }
+
+        return _Change::FIRST_VISIBLE_ROW;
+    }
+
+    return _Change::SELECTED_ROW;
+}
+
+bool CommonInspectTableView::_goUp(const Size steps)
+{
+    assert(_theRowCount > 0);
+
+    auto changed = false;
+
+    for (Index step = 0; step < steps; ++step) {
+        if (_theFirstVisibleRow == 0) {
+            return changed;
+        }
+
+        --_theFirstVisibleRow;
+        changed = true;
+    }
+
+    return changed;
+}
+
+bool CommonInspectTableView::_goDown(const Size steps)
+{
+    assert(_theRowCount > 0);
+
+    auto changed = false;
+
+    for (Index step = 0; step < steps; ++step) {
+        if (_theFirstVisibleRow == this->_maxFirstVisibleRow()) {
+            return changed;
+        }
+
+        ++_theFirstVisibleRow;
+        changed = true;
+    }
+
+    return changed;
+}
+
+bool CommonInspectTableView::_pageUp()
+{
+    return this->_goUp(_theMaxVisibleRowCount);
+}
+
+bool CommonInspectTableView::_pageDown()
+{
+    return this->_goDown(_theMaxVisibleRowCount);
+}
+
+bool CommonInspectTableView::_centerOnRow(const Index row)
+{
+    assert(_theRowCount > 0);
+
+    if (this->_isSinglePage()) {
+        return this->_firstVisibleRow(0);
+    }
+
+    const auto halfMaxVisibleRowCount = _theMaxVisibleRowCount / 2;
+
+    if (row <= halfMaxVisibleRowCount) {
+        return this->_firstVisibleRow(0);
+    }
+
+    return this->_firstVisibleRow(std::min(row - halfMaxVisibleRowCount,
+                                           this->_maxFirstVisibleRow()));
+}
+
+bool CommonInspectTableView::_centerOnSelRow()
+{
+    return this->_centerOnRow(_theSelRow);
+}
+
+Index CommonInspectTableView::_maxFirstVisibleRow() const noexcept
+{
+    assert(_theRowCount > 0);
+
+    if (this->_isSinglePage()) {
+        return 0;
+    }
+
+    return _theRowCount - _theMaxVisibleRowCount;
+}
+
+Size CommonInspectTableView::_visibleRowCount() const noexcept
+{
+    return _theRowCount <= _theMaxVisibleRowCount ? _theRowCount : _theMaxVisibleRowCount;
+}
+
+Index CommonInspectTableView::_lastVisibleRow() const noexcept
+{
+    assert(_theRowCount > 0);
+    return _theFirstVisibleRow + this->_visibleRowCount() - 1;
+}
+
+bool CommonInspectTableView::_rowIsVisible(const Index row) const noexcept
+{
+    assert(_theRowCount > 0);
+    assert(row < _theRowCount);
+    return row >= _theFirstVisibleRow && row < _theFirstVisibleRow + this->_visibleRowCount();
+}
+
+Index CommonInspectTableView::_yIndexFromVisibleRow(const Index row) const noexcept
+{
+    assert(this->_rowIsVisible(row));
+    return row - _theFirstVisibleRow;
+}
+
+Index CommonInspectTableView::_rowFromYIndex(const Index yIndex) const noexcept
+{
+    assert(_theRowCount > 0);
+    assert(_theFirstVisibleRow + yIndex < _theRowCount);
+    return _theFirstVisibleRow + yIndex;
+}
+
+bool CommonInspectTableView::_isSinglePage() const noexcept
+{
+    assert(_theRowCount > 0);
+    return this->_visibleRowCount() < _theMaxVisibleRowCount;
+}
+
+} // namespace jacques
---
+++ jacquesctf/inspect-common/common-inspect-table-view.hpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2022 Philippe Proulx <eepp.ca> - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium, is strictly
+ * prohibited. Proprietary and confidential.
+ */
+
+#ifndef _JACQUES_INSPECT_COMMON_COMMON_INSPECT_TABLE_VIEW_HPP
+#define _JACQUES_INSPECT_COMMON_COMMON_INSPECT_TABLE_VIEW_HPP
+
+#include <cassert>
+#include <utility>
+#include <tuple>
+
+#include "aliases.hpp"
+
+namespace jacques {
+
+class CommonInspectTableView
+{
+protected:
+    enum class _Change
+    {
+        NONE,
+        SELECTED_ROW,
+        FIRST_VISIBLE_ROW,
+    };
+
+protected:
+    CommonInspectTableView() = default;
+    void _updateCounts(Size rowCount, Size maxVisibleRowCount);
+    bool _firstVisibleRow(Index firstVisibleRow);
+    _Change _selFirstRow(bool ensureVisible = false);
+    _Change _selLastRow(bool ensureVisible = false);
+    _Change _selNextRow(Size steps = 1, bool ensureVisible = false);
+    _Change _selPrevRow(Size steps = 1, bool ensureVisible = false);
+    _Change _selRow(Index row, bool ensureVisible = false);
+    bool _goUp(Size steps = 1);
+    bool _goDown(Size steps = 1);
+    bool _pageUp();
+    bool _pageDown();
+    bool _centerOnRow(Index row);
+    bool _centerOnSelRow();
+    Index _maxFirstVisibleRow() const noexcept;
+    Size _visibleRowCount() const noexcept;
+    Index _lastVisibleRow() const noexcept;
+    bool _rowIsVisible(Index row) const noexcept;
+    Index _yIndexFromVisibleRow(Index row) const noexcept;
+    Index _rowFromYIndex(Index yIndex) const noexcept;
+    bool _isSinglePage() const noexcept;
+
+    bool _rowIsSel(const Index row) const noexcept
+    {
+        return row == _theSelRow;
+    }
+
+    Size _maxVisibleRowCount() const noexcept
+    {
+        return _theMaxVisibleRowCount;
+    }
+
+    Index _selRow() const noexcept
+    {
+        return _theSelRow;
+    }
+
+    Index _firstVisibleRow() const noexcept
+    {
+        return _theFirstVisibleRow;
+    }
+
+private:
+    template <typename FuncT>
+    _Change _selRow(const Size steps, const bool ensureVisible, FuncT&& func)
+    {
+        assert(_theRowCount > 0);
+
+        auto change = _Change::NONE;
+
+        for (Index step = 0; step < steps; ++step) {
+            const auto stepChange = this->_selRow(func(), ensureVisible);
+
+            if (stepChange == _Change::NONE) {
+                // nothing more to do
+                return change;
+            }
+
+            if (static_cast<int>(stepChange) > static_cast<int>(change)) {
+                // more important change
+                change = stepChange;
+            }
+        }
+
+        return change;
+    }
+
+private:
+    Size _theRowCount = 0;
+    Size _theMaxVisibleRowCount = 0;
+    Index _theFirstVisibleRow = 0;
+    Index _theSelRow = 0;
+};
+
+} // namespace jacques
+
+#endif // _JACQUES_INSPECT_COMMON_COMMON_INSPECT_TABLE_VIEW_HPP
