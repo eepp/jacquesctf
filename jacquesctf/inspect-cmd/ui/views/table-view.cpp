1585dea5222308fc9cf1c60188246aaaa276b3af
Philippe Proulx <eeppeliteloop@gmail.com>
2025-06-27 09:50:08 -0400

    Remove illegitimate debugging file writing
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- jacquesctf/inspect-cmd/ui/views/table-view.cpp
+++ jacquesctf/inspect-cmd/ui/views/table-view.cpp
@@ -217,11 +217,6 @@ void TableView::_drawCell(const Point& contentPos, const TableViewColumnDescr& d
 
         std::sprintf(buf.data(), fmt, parts.first.c_str(), parts.second.c_str());
 
-        auto fh = fopen("/tmp/meow", "wb");
-
-        fwrite(parts.first.c_str(), 1, parts.first.size() + 1, fh);
-        fclose(fh);
-
         if (customStyle) {
             this->_stylist().tableViewTextCell(*this, cell.emphasized());
         }
